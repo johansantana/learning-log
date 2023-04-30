bf1f9090c05965d31c0e283a3ffef2959b4daa2a
Johan Santana <johancarlosst@gmail.com>
2023-04-30 10:34:28 -0400

    fix: DEBUG get

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(2t52w2d3-%+7bzlwc7f9w_i=3_*!y=#rfv8rze17o+hz)=3wd'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = not 'DEBUG' in os.environ.get('DEBUG')
+DEBUG = not 'DEBUG' in os.environ
 
 if not DEBUG:
     DEBUG = os.environ.get('DEBUG')
