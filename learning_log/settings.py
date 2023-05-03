8be1d71d2e6891f2f347893b20dffd0084d82616
Johan Santana <johancarlosst@gmail.com>
2023-05-03 12:40:35 -0400

    refactor: default database on dev

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -80,20 +80,12 @@ WSGI_APPLICATION = 'learning_log.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        # Feel free to alter this value to suit your needs.
+        default='sqlite:///db.sqlite3',
+        conn_max_age=600
+    )
 }
-
-if not DEBUG:
-    DATABASES = {
-        'default': dj_database_url.config(
-            # Feel free to alter this value to suit your needs.
-            default='postgresql://postgres:postgres@localhost:5432/mysite',
-            conn_max_age=600
-        )
-    }
 
 
 # Password validation
